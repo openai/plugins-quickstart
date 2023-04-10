425eb6fb7fe48930b50238df46bebfccbd2e4869
Logan Kilpatrick <logan@openai.com>
2023-04-10 12:32:54 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Plugins Quickstart
+
+Get a Todo list ChatGPT plugin up and running in under 5 minutes using Python. If you do not already have plugin developer access, please [join the waitlist](https://openai.com/waitlist/plugins).
+
+## Setup
+
+To install the required packages for this plugin, run the following command:
+
+```bash
+pip install -r requirements.txt
+```
+
+To run the plugin, enter the following command:
+
+```bash
+python main.py
+```
+
+Once the local server is running:
+
+1. Navigate to https://chat.openai.com. 
+2. In the Model drop down, select "Plugins" (note, if you don't see it there, you don't have access yet).
+3. Select "Plugin store"
+4. Select "Develop your own plugin"
+5. Enter in `localhost:5003` since this is the URL the server is running on locally, then select "Find manifest file".
+
+The plugin should now be installed and enabled! You can start with a question like "What is on my todo list" and then try adding something to it as well! 
+
+## Getting help
+
+If you run into issues or have questions building a plugin, please join our [Developer community forum](https://community.openai.com/c/chat-plugins/20).
---
+++ main.py
@@ -0,0 +1,56 @@
+import json
+
+import quart
+import quart_cors
+from quart import request
+
+app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.com")
+
+# Keep track of todo's. Does not persist if Python session is restarted.
+_TODOS = {}
+
+@app.post("/todos/<string:username>")
+async def add_todo(username):
+    request = await quart.request.get_json(force=True)
+    if username not in _TODOS:
+        _TODOS[username] = []
+    _TODOS[username].append(request["todo"])
+    return quart.Response(response='OK', status=200)
+
+@app.get("/todos/<string:username>")
+async def get_todos(username):
+    return quart.Response(response=json.dumps(_TODOS.get(username, [])), status=200)
+
+@app.delete("/todos/<string:username>")
+async def delete_todo(username):
+    request = await quart.request.get_json(force=True)
+    todo_idx = request["todo_idx"]
+    # fail silently, it's a simple plugin
+    if 0 <= todo_idx < len(_TODOS[username]):
+        _TODOS[username].pop(todo_idx)
+    return quart.Response(response='OK', status=200)
+
+@app.get("/logo.png")
+async def plugin_logo():
+    filename = 'logo.png'
+    return await quart.send_file(filename, mimetype='image/png')
+
+@app.get("/.well-known/ai-plugin.json")
+async def plugin_manifest():
+    host = request.headers['Host']
+    with open("./.well-known/ai-plugin.json") as f:
+        text = f.read()
+        return quart.Response(text, mimetype="text/json")
+
+@app.get("/openapi.yaml")
+async def openapi_spec():
+    host = request.headers['Host']
+    with open("openapi.yaml") as f:
+        text = f.read()
+        return quart.Response(text, mimetype="text/yaml")
+
+def main():
+    app.run(debug=True, host="0.0.0.0", port=5003)
+
+if __name__ == "__main__":
+    main()
