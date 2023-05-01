aab7bcad8dcab1eedf4e59e1552098f63684bb8c
Rizel Scarlett <blackgirlbytes@github.com>
2023-05-01 18:17:35 -0400

    no need to replace strings for local setup
    Co-authored-by: Samruddhi Khandale <samruddhikhandale@github.com>

--- .devcontainer/addcodespacename.sh
+++ .devcontainer/addcodespacename.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 set -e
 # Determine the value of SITE_HOST based on whether the project is opened in a Codespace
-if [ -z "${CODESPACE_NAME+x}" ]; then
-	SITE_HOST="http://localhost:5003"
-else
-	SITE_HOST="https://${CODESPACE_NAME}-5003.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
+if [ -n "$CODESPACE_NAME" ]; then
+        SITE_HOST="https://${CODESPACE_NAME}-5003.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
+
+        # Replace "localhost:5003" with the value of SITE_HOST in the ai-plugin.json file
+        sed -i "s#http://localhost:5003#${SITE_HOST}#g" .well-known/ai-plugin.json
+
+        # Replace "localhost:5003" with the value of SITE_HOST in the openapi.yaml file
+        sed -i "s#http://localhost:5003#${SITE_HOST}#g" openapi.yaml
 fi
 
-# Replace "localhost:5003" with the value of SITE_HOST in the ai-plugin.json file
-sed -i "s#http://localhost:5003#${SITE_HOST}#g" .well-known/ai-plugin.json
-
-# Replace "localhost:5003" with the value of SITE_HOST in the openapi.yaml file
-sed -i "s#http://localhost:5003#${SITE_HOST}#g" openapi.yaml
