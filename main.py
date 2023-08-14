5942af668a5acc37e80aa8ed0a978a06a7ef4bdd
billgui <qiang.gui@compass.com>
2023-08-14 13:31:09 -0700

    adding listing type in the parameters

--- main.py
+++ main.py
@@ -115,6 +115,14 @@ async def query(username):
         print(imageUrl)
         listing = {"Display URL": landing_url, "type": listingType, "price": price, "bedrooms": bedroomsNumber, "bathrooms": totalBathrooms, "square feet": sqft, "thumbnail": imageUrl}
         properties.append(listing)
+
+    if request.get("listingType") is not None:
+       listingType = request.get("listingType")
+       if listingType == 'rental':
+          properties.append("More listings : https://www.compass.com/for-rent/")
+       else:
+          properties.append("More listings : https://www.compass.com/homes-for-sale/")
+
     return quart.Response(response=json.dumps(properties), status=200)
 
 
