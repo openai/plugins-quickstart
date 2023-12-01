907470b39fdc84a42ee6440310b23a5e46025092
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2023-12-01 23:02:04 +0000

    temp URLs

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ async def openapi_spec():
         return quart.Response(text, mimetype="text/yaml")
 
 def main():
-    app.run(debug=True, host="0.0.0.0", port=5003)
+    app.run(debug=True, host="localhost", port=5003)
 
 if __name__ == "__main__":
     main()
